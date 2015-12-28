78e81393d9cbd0e84c9b9a3a4f948c08844b8e20
Adão Júnior <itsjunnior@gmail.com>
2015-12-28 20:22:21 -0200

    remove unused import

--- web/main.dart
+++ web/main.dart
@@ -2,7 +2,6 @@ import 'package:angular2/bootstrap.dart';
 import 'package:english-verbs/app.dart';
 import 'package:angular2/angular2.dart' show provide;
 import 'package:angular2/router.dart' show ROUTER_PROVIDERS,APP_BASE_HREF, LocationStrategy, HashLocationStrategy;
-import 'dart:html' show window;
 import 'package:usage/usage_html.dart' show AnalyticsHtml, Analytics;
 
 const String id = 'UA-71715642-3';
