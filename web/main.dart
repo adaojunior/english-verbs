1f54a0271c809d56f6044da759d22488815c63d1
Adão Júnior <itsjunnior@gmail.com>
2016-01-02 19:38:39 -0200

    update analytics id

--- web/main.dart
+++ web/main.dart
@@ -4,7 +4,7 @@ import 'package:angular2/angular2.dart' show provide;
 import 'package:angular2/router.dart' show ROUTER_PROVIDERS,APP_BASE_HREF, LocationStrategy, HashLocationStrategy;
 import 'package:usage/usage_html.dart' show AnalyticsHtml, Analytics;
 
-const String id = 'UA-71715642-3';
+const String id = 'UA-71715642-4';
 const String name = 'English-Verbs';
 const String version = '0.3.0';
 
