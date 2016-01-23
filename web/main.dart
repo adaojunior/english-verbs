12c63ab718f808c252415ab5c3d6f5cfd7138d0a
Adão Júnior <itsjunnior@gmail.com>
2016-01-23 12:25:08 -0200

    change version

--- web/main.dart
+++ web/main.dart
@@ -11,7 +11,7 @@ import 'package:usage/usage_html.dart' show AnalyticsHtml, Analytics;
 
 const String id = 'UA-71715642-4';
 const String name = 'English-Verbs';
-const String version = '0.3.4';
+const String version = '0.3.5';
 
 main() => bootstrap(AppComponent, [
       ROUTER_PROVIDERS,
