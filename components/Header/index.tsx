0532f6d538635ac97594b5e9d012d7952e8a1f04
Adão Pardim Jr <adao.pardim@ae.studio>
2023-01-02 17:34:21 -0300

    feat: conjugation table header and navigation

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,13 +1,7 @@
-import { VerbTensesTabs } from './VerbTensesTabs'
-
 export function Header() {
   return (
     <div>
       <div className="h-16 bg-teal-700"></div>
-      <div className="bg-teal-600 text-white">
-        <div className="py-4 text-center text-4xl">To be</div>
-        <VerbTensesTabs />
-      </div>
     </div>
   )
 }
