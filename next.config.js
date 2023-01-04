3a18422b41378de27bd04e7a7ee8b770c9e05974
Adão Júnior <adaojunior@users.noreply.github.com>
2023-01-04 11:31:55 -0300

    chore: add support to Sentry (#20)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
+const { withSentryConfig } = require('@sentry/nextjs')
+
+const enableSentryWebpackPlugin =
+  process.env.SENTRY_ENABLE_WEBPACK_PLUGIN === 'true'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sentry: {
+    hideSourceMaps: true,
+    disableServerWebpackPlugin: !enableSentryWebpackPlugin,
+    disableClientWebpackPlugin: !enableSentryWebpackPlugin,
+  },
 }
 
-module.exports = nextConfig
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(nextConfig, {
+  silent: true,
+})
