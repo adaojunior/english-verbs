a400ad21d88a7ae7da9cb5271e61a97044145139
Adão Júnior <adaojunior@users.noreply.github.com>
2023-01-03 22:59:19 -0300

    feat: allow searching verbs (#19)
    * feat: allow searching verbs
    
    * fix: search value should always be lowercase
    
    * fix: remove input outline on safari
    
    * feat: remove focus after search
    
    * fix: prevent zooming in on input focus on mobile

---
+++ components/Header/SearchInput.tsx
@@ -0,0 +1,55 @@
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+import { useRouter } from 'next/router'
+import { ChangeEvent, KeyboardEvent, useState } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+export function SearchInput() {
+  const router = useRouter()
+  const [value, setValue] = useState('')
+  const [focused, setFocused] = useState(false)
+
+  const onFocus = () => setFocused(true)
+  const onBlur = () => setFocused(false)
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) =>
+    setValue(event.target.value?.trimStart())
+
+  const onKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      const search = value.trim().toLowerCase()
+      search && router.push(`/verb/${search}`)
+      event.currentTarget.blur()
+    }
+  }
+
+  return (
+    <div
+      className={twMerge(
+        'relative mx-auto flex h-10 w-full items-center rounded-sm md:w-[400px]',
+        focused ? 'bg-white' : 'bg-white/[.1]'
+      )}
+    >
+      <MagnifyingGlassIcon
+        className={twMerge(
+          'absolute left-3 z-0 h-5',
+          focused ? 'fill-slate-800' : 'fill-white'
+        )}
+      />
+      <input
+        className={twMerge(
+          'z-10 w-full bg-transparent pl-10 text-sm outline-none',
+          focused
+            ? 'text-slate-800 placeholder:text-slate-800'
+            : 'text-white placeholder:text-white'
+        )}
+        type="text"
+        placeholder="Search verb"
+        value={value}
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        onKeyDown={onKeyDown}
+      />
+    </div>
+  )
+}
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,7 +1,11 @@
+import { SearchInput } from '@/components/Header/SearchInput'
+
 export function Header() {
   return (
     <div>
-      <div className="h-16 bg-teal-700"></div>
+      <div className="flex h-16 items-center bg-teal-700 px-2.5">
+        <SearchInput />
+      </div>
     </div>
   )
 }
