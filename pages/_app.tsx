283430199d131b6707bd18cab68fbb302304949c
Adão Pardim Jr <adao.pardim@ae.studio>
2023-01-03 23:32:02 -0300

    fix: viewport meta tags should not be used in _document.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Layout from '../components/Layout'
+import Head from 'next/head'
+import Layout from '@/components/Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
