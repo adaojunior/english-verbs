65d2d47926dffa40cd225bba2ebfa96f9f32c9a8
Adão Pardim Jr <adao.pardim@ae.studio>
2023-01-04 17:15:07 -0300

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head'
+
 export default function Custom404() {
   return (
-    <div className="m-auto flex max-w-4xl justify-center pt-10">
-      <h1>404 - Page Not Found</h1>
-    </div>
+    <>
+      <Head>
+        <title>404: This page could not be found.</title>
+      </Head>
+
+      <div className="m-auto flex max-w-4xl justify-center pt-10">
+        <h1>404 - This page could not be found.</h1>
+      </div>
+    </>
   )
 }
