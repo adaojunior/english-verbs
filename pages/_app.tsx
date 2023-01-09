452f78ab842562f71e93af2b6a68a224d145f6cd
Adão Pardim Jr <adao.pardim@ae.studio>
2023-01-09 20:33:53 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,6 +17,16 @@ export default function App({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1"
         />
+        <meta name="application-name" content="English Verbs" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="English Verbs" />
+        <meta
+          name="description"
+          content="Learn how to conjugate English verbs."
+        />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#0f766e" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
